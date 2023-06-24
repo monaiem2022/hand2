12686d53ace9c59397b5d8da0c2b7e33ee41d2c7
monaiem2022 <95927663+monaiem2022@users.noreply.github.com>
2023-06-25 01:26:58 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -4,6 +4,8 @@ var all1 = document.getElementById('all1');
 var client1 = document.getElementById('client1');
 var all2 = document.getElementById('all2');
 
+var all3 = document.getElementById('all3');
+
 
 
 
@@ -22,7 +24,8 @@ button.onclick = function () {
 
 
 client1.onclick = function () {
-    all2.style.display ='none'
+    all2.style.display ='none';
+    all3.style.display = 'block'
 }
 
 
